dfd67e03bae80e1c8acf0cf9ede9c213a56dceaa
José Miguel <474643+jm2dev@users.noreply.github.com>
2019-04-23 14:44:20 +0100

    PKATOPS-2105 display internal ip address.

--- roles/stats/files/my_ip_address.sh
+++ roles/stats/files/my_ip_address.sh
@@ -8,6 +8,6 @@ echo "body { background: #2e3440 }" >> $outfile
 echo "section { font-size: 12em; color: #ebcb8b; border-radius: 1em; padding: 1em; position: absolute; top: 50%; left: 50%; margin-right: -50%; transform: translate(-50%, -50%) }" >> $outfile
 echo "</style>" >> $outfile
 echo "<section>" >> $outfile
-curl ipinfo.io/ip >> $outfile
+hostname -I | awk '{ print $1 }' >> $outfile
 echo "</section>" >> $outfile
 echo "</html>" >> $outfile
