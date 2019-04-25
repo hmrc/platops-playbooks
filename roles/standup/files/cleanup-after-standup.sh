e5147460a093675e5dc77a3ddb4e2f339d986a5e
Dario Ghunney Ware <username@users.noreply.github.com>
2019-04-25 15:55:43 +0100

    PLATOPS-2105 Adding standup scripts to optiplex7010

--- roles/standup/files/cleanup-after-standup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-killall chromium-browser 
-sleep 5
-export DISPLAY=:0
-chromium-browser --start-maximized &
-sleep 20
-DISPLAY=:0 xdotool key F11
-
--- roles/standup/files/countdown-to-standup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-killall chromium-browser
-sleep 5
-export DISPLAY=:0
-chromium-browser --start-maximized file:///home/pi/countdown.html &
-sleep 20
-DISPLAY=:0 xdotool key F11
-
